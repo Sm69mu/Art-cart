1e29f3bb99518f177d070f35b6afec6aeb6141d6
Sm69mu <soumyajitmukherjee271@gmail.com>
2023-11-14 16:47:43 +0530

    Updated the cart screen

--- lib/screens/default screens/cart screen/cart_screen.dart
+++ lib/screens/default screens/cart screen/cart_screen.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce_application/constants/default%20consts/consts.dart';
+import 'package:e_commerce_application/widgets/custom%20buttons/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class CartScreen extends StatelessWidget {
@@ -7,11 +8,35 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: " Cart".text.fontFamily(bold).size(22).make(),
-      ),
-      body: "cart is empty".text.fontFamily(bold).size(30).makeCentered(),
-    );
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: " Cart".text.fontFamily(bold).size(22).make(),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.shopping_cart_outlined,
+              color: brownColor,
+              size: 150,
+            ),
+            10.heightBox,
+            "Your Cart is Empty"
+                .text
+                .fontFamily(bold)
+                .color(fontGrey)
+                .size(25)
+                .make(),
+            25.heightBox,
+            customButton(
+                    title: " Start Shopping ",
+                    color: brownColor,
+                    textColor: whiteColor,
+                    onPress: () {})
+                .box
+                .width(150)
+                .make()
+          ],
+        ).box.makeCentered());
   }
 }
